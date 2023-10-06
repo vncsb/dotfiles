39ba3fefc87a5dd674d848a34c9da662cb288372
Vinicius Bernardino <viniciusbernardino1@hotmail.com>
2023-10-06 11:11:58 -0300

    Revert "[NVIM] not installing plugins on startup"
    This reverts commit 917fd8b65f184113585f434d2dd13096a81cacb4.

--- .config/nvim/lua/Lazy.lua
+++ .config/nvim/lua/Lazy.lua
@@ -1,13 +1,13 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+  vim.fn.system {
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,10 +16,7 @@ vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappin
 
 -- load lazy
 require("lazy").setup("user", {
-  install = {
-    missing = false,
-    colorscheme = { require("user.colorscheme").name },
-  },
+  install = { colorscheme = { require("user.colorscheme").name } },
   defaults = { lazy = true, version = "57cce98dfdb2f2dd05a0567d89811e6d0505e13b" },
   ui = { wrap = "true" },
   change_detection = { enabled = true },

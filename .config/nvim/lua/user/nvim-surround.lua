d0c2311f1d8998d3e115643a8b4675b8df852529
Vinicius Bernardino <viniciusbernardino1@hotmail.com>
2024-05-31 21:22:13 -0300

    [NVIM] Adding nvim-surround

---
+++ .config/nvim/lua/user/nvim-surround.lua
@@ -0,0 +1,11 @@
+local M = {
+	"kylechui/nvim-surround",
+	commit = "f1f0699a1d49f28e607ffa4361f1bbe757ac5ebc",
+	event = "VeryLazy",
+}
+
+function M.config()
+	require("nvim-surround").setup({})
+end
+
+return M

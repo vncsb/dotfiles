c22270f80670d269b522b78fefedd2ebca81d2c4
Vinicius Bernardino <viniciusbernardino1@hotmail.com>
2024-06-06 18:16:42 -0300

    [NVIM] tweaking img-clip and telescope configs

--- .config/nvim/lua/user/img-clip.lua
+++ .config/nvim/lua/user/img-clip.lua
@@ -1,10 +1,19 @@
 local M = {
-  "HakonHarnes/img-clip.nvim",
-  commit = "d7ddfd88d50f0b1acd5175448ad159057fe41ab6",
-  event = "VeryLazy",
-  keys = {
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-  },
+	"HakonHarnes/img-clip.nvim",
+	commit = "d7ddfd88d50f0b1acd5175448ad159057fe41ab6",
+	event = "VeryLazy",
+	keys = {
+		{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+	},
 }
+
+function M.config()
+	require("img-clip").setup({
+		default = {
+			dir_path = "images",
+      relative_to_current_file = true,
+		},
+	})
+end
 
 return M
